2e312d2c81cdb069be681b292bed3256d2cb6914
Martins <victorcostamartins0@gmail.com>
2025-05-27 01:12:05 -0300

    oranização do projeto

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        
+        <!-- Styles -->
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+      
+    </head>
+    <body>
+        @yield('contet')
+
+        <!-- <footer>
+            <p>Copyright ©  Carrinho de compra LTDA</p>
+        </footer> -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,80 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.main')
 
-        <title>Laravel</title>
+@section('title', 'Painel')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('contet')
 
-        <!-- Styles -->
-      
-    </head>
+    <div>
     
-</html>
+        <nav class="navbar navbar-light bg-light;" style=" background: #fff ">
+            <div class="container-fluid d-flex justify-content-center align-items-center">
+                <form class="d-flex align-items-center justify-content-center border rounded-pill" style="min-width: 400px; height: 2rem;" >
+                    <input style="width: 70%; border:none" type="text" placeholder="Search" > |
+                    <button style=" border:none; background: #fff " type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        <div style="position:static">
+            <div style="width:90%; height:350px; margin:auto; position: relative;
+    top: 16px;">
+            
+            </div>
+
+            <div class="border border-black mx-auto" style=" width: 78%; position: relative; top: -38px;">
+            
+                <!-- Tecnologia -->
+                <div class="container my-4">
+
+                        <div id="carouselCards" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+
+                                <div class="carousel-item active">
+                                    <div class="row justify-content-center">
+                                    @for ($i = 0; $i < 4; $i++)
+                                        <div class="col-md-3 d-flex align-items-center justify-content-center flex-column border text-center mx-1" style="width: 150px; border-color: black;">
+                                        <img id="img_card" src="{{ asset('img/monitor.png') }}" alt="">
+                                        <h6>Monitor 19 Preto Led</h6>
+                                        <p>R$279</p>
+                                        </div>
+                                    @endfor
+                                    </div>
+                                </div>
+
+                                <div class="carousel-item">
+                                    <div class="row justify-content-center">
+                                    @for ($i = 0; $i < 3; $i++)
+                                        <div class="col-md-3 d-flex align-items-center justify-content-center flex-column border text-center mx-1" style="width: 150px; border-color: black;">
+                                        <img id="img_card" src="{{ asset('img/monitor.png') }}" alt="">
+                                        <h6>Monitor 19 Preto Led</h6>
+                                        <p>R$279</p>
+                                        </div>
+                                    @endfor
+                                    </div>
+                                </div>
+
+                                
+
+                            </div>
+
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselCards" data-bs-slide="prev">
+                            <!-- <span class="carousel-control-prev-icon"> > </span> -->
+                            <i class="bi bi-chevron-compact-left"></i>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselCards" data-bs-slide="next">
+                            <span class="carousel-control-next-icon"> < </span>
+                            </button>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+                <!-- Livro -->
+
+                <!-- Casa -->
+
+                <!-- Automovel -->
+        </div>
+    </div>
+
+@endsection
